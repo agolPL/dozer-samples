69938252458d501675b658d2436d5a3a379c0c35
agolPL <andiPansa@gmail.com>
2014-03-06 13:27:53 +0100

    simple dozer mapping

---
+++ src/test/java/pl/agol/dozer/test/PersonMappingTest.java
@@ -0,0 +1,45 @@
+package pl.agol.dozer.test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pl.agol.dozer.test.entity.OtherPerson;
+import pl.agol.dozer.test.entity.Person;
+
+/**
+ * 
+ * @author Andrzej Goławski
+ * 
+ */
+public class PersonMappingTest {
+
+	private Mapper mapper;
+
+	@BeforeClass
+	public void setup() {
+		mapper = new DozerBeanMapper();
+	}
+
+	@Test
+	public void should_create_OtherPerson_from_given_Person() {
+
+		// given
+		Person person = new Person()
+			.hasAge(Person.PERSON_AGE)
+			.hasLastname(Person.PERSON_LASTNAME)
+			.hasName(Person.PERSON_NAME);
+
+		// when
+		OtherPerson otherPerson = mapper.map(person, OtherPerson.class);
+
+		// than
+		assertEquals(otherPerson.getAge(), Person.PERSON_AGE);
+		assertEquals(otherPerson.getName(), Person.PERSON_NAME);
+		assertEquals(otherPerson.getLastname(), Person.PERSON_LASTNAME);
+	}
+
+}
