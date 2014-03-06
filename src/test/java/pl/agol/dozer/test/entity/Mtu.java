19e57c4cadd5724ae3f7b107c1a402f432a0c62b
agolPL <andiPansa@gmail.com>
2014-03-06 12:13:38 +0100

    added mtu class

---
+++ src/test/java/pl/agol/dozer/test/entity/Mtu.java
@@ -0,0 +1,40 @@
+package pl.agol.dozer.test.entity;
+
+/**
+ * 
+ * @author Andrzej Goławski
+ * 
+ */
+public class Mtu {
+
+	private String jina;
+
+	private String jinaLaMwisho;
+
+	private Integer umri;
+
+	public String getJina() {
+		return jina;
+	}
+
+	public void setJina(String jina) {
+		this.jina = jina;
+	}
+
+	public String getJinaLaMwisho() {
+		return jinaLaMwisho;
+	}
+
+	public void setJinaLaMwisho(String jinaLaMwisho) {
+		this.jinaLaMwisho = jinaLaMwisho;
+	}
+
+	public Integer getUmri() {
+		return umri;
+	}
+
+	public void setUmri(Integer umri) {
+		this.umri = umri;
+	}
+
+}
